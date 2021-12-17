55b1af691ae6f0833c57a1800087e2f2c32f3b2a
Harry <hrgus3@gmail.com>
2021-12-16 20:59:07 -0500

    forEach made, but needs filled

--- index.js
+++ index.js
@@ -32,49 +32,24 @@ function search(e) {
 
    fetch(`https://musicbrainz.org/ws/2/release/?query=${queryValue}&fmt=json`)
    .then(resp => resp.json())
-   .then(result => console.log(result))
+   .then((result) => { 
+      searchResultInArray = search;
+      // console.log(searchResultInArray);
+      return result
+   })
+}
+// Event Listener(s)
+searchForm.addEventListener('submit', search)
+
+
+
+function createResultsBox() {
+   resultsBox = document.querySelector('#searchResults');
+   resultsBox.foreach(async function(){
+      
+   })
+
 }
 
-searchForm.addEventListener('submit', search);
-
-// Changes XML to JSON
-// function xmlToJson(xml) {
-	
-// 	// Create the return object
-// 	var obj = {};
-
-// 	if (xml.nodeType == 1) { // element
-// 		// do attributes
-// 		if (xml.attributes.length > 0) {
-// 		obj["@attributes"] = {};
-// 			for (var j = 0; j < xml.attributes.length; j++) {
-// 				var attribute = xml.attributes.item(j);
-// 				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-// 			}
-// 		}
-// 	} else if (xml.nodeType == 3) { // text
-// 		obj = xml.nodeValue;
-// 	}
-
-// 	// do children
-// 	if (xml.hasChildNodes()) {
-// 		for(var i = 0; i < xml.childNodes.length; i++) {
-// 			var item = xml.childNodes.item(i);
-// 			var nodeName = item.nodeName;
-// 			if (typeof(obj[nodeName]) == "undefined") {
-// 				obj[nodeName] = xmlToJson(item);
-// 			} else {
-// 				if (typeof(obj[nodeName].push) == "undefined") {
-// 					var old = obj[nodeName];
-// 					obj[nodeName] = [];
-// 					obj[nodeName].push(old);
-// 				}
-// 				obj[nodeName].push(xmlToJson(item));
-// 			}
-// 		}
-// 	}
-// 	return obj;
-// };
-
-// xmlToJson(h);
+resultsBox = document.querySelector('#searchResults');
 
