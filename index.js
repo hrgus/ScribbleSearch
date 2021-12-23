7bc2d26132c9eb375399b8faa36a44e4df6c69e0
Harry <hrgus3@gmail.com>
2021-12-23 12:29:02 -0500

    Got functions working

--- index.js
+++ index.js
@@ -21,26 +21,26 @@ const brewList = document.getElementById('search-list');
    
    
    
-   function searchOptionsFunc(){
-      let optionEl1 = document.createElement('OPTION');
-      let optionEl2 = document.createElement('OPTION');
-      let optionEl3 = document.createElement('OPTION');
+//    function searchOptionsFunc(){
+//       let optionEl1 = document.createElement('OPTION');
+//       let optionEl2 = document.createElement('OPTION');
+//       let optionEl3 = document.createElement('OPTION');
       
-      optionEl1.textContent = "Artist"
-      optionEl2.textContent = "Album"
-      optionEl3.textContent = "Song"
+//       optionEl1.textContent = "Artist"
+//       optionEl2.textContent = "Album"
+//       optionEl3.textContent = "Song"
       
-      searchOptions.appendChild(optionEl1);
-      searchOptions.appendChild(optionEl2);
-searchOptions.appendChild(optionEl3);
+//       searchOptions.appendChild(optionEl1);
+//       searchOptions.appendChild(optionEl2);
+//        searchOptions.appendChild(optionEl3);
 
-}
+// }
 
 function search(e) {
    e.preventDefault();
    
    let query = document.querySelector('#search-Input');
-   let queryValue = query.value
+   let queryValue = query.value;
    console.log('what was searched:', queryValue)
    
    searchForm.appendChild(searchOptions);
@@ -48,22 +48,29 @@ function search(e) {
    searchForm.reset();
    
    fetch(`https://musicbrainz.org/ws/2/release/?query=${queryValue}&fmt=json`)
-   .then(resp => resp.json())
-   .then((results) => {results.forEach(results => renderSongInfoBox(results))})
-   // console.log(result);
+   .then(results => results.json())
+   .then((resultsObj) => {
+      // console.log(resultsObj)
+      resultsArray = resultsObj['releases'];
+      return resultsArray.forEach(renderSongInfoBox);
+      
+   })
+   // console.log(results);
    // console.log(result['releases']); 
 }
 
-function renderSongInfoBox(results) {
+function renderSongInfoBox(resultObj) {
    
    const resultsBox = document.createElement('div');
    const resultsBoxHead = document.createElement('h2');
    const resultsContainer = document.createElement('p');
-
+   const body = document.querySelector('#body');
    
-   resultsBox.className = "resultBox"
-   resultsBoxHead.textContent = "Search Result:";
-   resultsContainer.textContent = results;
+   body.appendChild(resultsBox);
+   debugger
+   resultsBox.className = "resultBox";
+   resultsBoxHead.textContent = resultObj.title;
+   resultsContainer.textContent = resultObj['artist-credit'][0].artist.name;
    resultsBox.appendChild(resultsBoxHead);
    resultsBox.appendChild(resultsContainer); 
 
