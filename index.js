a71451bfb35128a5e98f553ffe20f4aef7d96a26
Harry Gustafson <hrgus3@gmail.com>
2021-12-25 00:52:02 -0500

    Added save icon

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const brewList = document.getElementById('search-list');
    const searchTypeBtn = document.querySelector('#searchTypeBtn');
    const searchOptions = document.createElement('select', '#selectOptions');
   
-   
+   const refreshResultBtn = document.querySelector('#refreshResultBtn');
 
 
 function search(e) {
@@ -33,35 +33,53 @@ function search(e) {
       console.log(resultsArray);
       return resultsArray.forEach(renderSongInfoBox);
    } )
-   // console.log(result['releases']); 
+   
+   
 }
 
+
+// const for selecting the body
+const body = document.querySelector('#body');
+// const for selecting the Div that contains the <ol>
+const resultsDiv = document.createElement('Div');
+resultsDiv.setAttribute("id", "resultsDiv");
+const refreshResultBtnEl = document.createElement('button');
+refreshResultBtnEl.setAttribute("id", "refreshResultBtn");
+refreshResultBtnEl.textContent = "Refresh";
+resultsDiv.appendChild(refreshResultBtnEl);
+const resultsOlEl = document.createElement('ol');
+resultsOlEl.setAttribute("id", "resultsOl");
+resultsDiv.appendChild(resultsOlEl);
+body.appendChild(resultsDiv);
+
+
 function renderSongInfoBox(resultObj) {
-   // const for selecting the body
-   // const body = document.querySelector('#body');
-   // const for selecting the Div that contains the <ol>
-   // const resultsDiv = document.querySelector('#resultsDiv');
    
    // const for selecting the <ol> which contains the <li> items 
-   const resultsOl = document.querySelector('#resultsOl');
-   
+   // const resultsOl = document.querySelector('#resultsOl');
+
+   const unSavedIcon = document.createElement('Button') 
+   unsa 
+   "\u002B";
+
    //const for creating <li> items
    const resultsListItem = document.createElement('li');
    // Appending each <li> to the <ol>
-   resultsOl.appendChild(resultsListItem);
+   resultsOlEl.appendChild(resultsListItem);
    // const for creating the <h4> for each <li> item
    const resultsListItemHead = document.createElement('h4');
+   resultsListItemHead.setAttribute("id", "resultListItemH4")
    // assigning the textContent of the <h4> Elements
-   resultsListItemHead.textContent = resultObj.title;
+   resultsListItemHead.textContent = resultObj.title + " " + unSavedIcon;
    // Appending the <h4> to the <li> items
    resultsListItem.appendChild(resultsListItemHead);
    // const for creating the <p> for each <li> item
    const resultsParagraph = document.createElement('p');
+   resultsParagraph.setAttribute("id", "resultListItemP");
    // assigning the textContent of the <p> Elements
    resultsParagraph.textContent = '- ' + resultObj['artist-credit'][0].artist.name;
    // Appending the <p> the <li> Element
    resultsListItem.appendChild(resultsParagraph);
-   
 
    // function reset 
    
@@ -69,12 +87,14 @@ function renderSongInfoBox(resultObj) {
 
 
 
-
 // Event Listener(s)
 //-------------------------------------------
    searchForm.addEventListener('submit', search)
    searchTypeBtn.addEventListener('click', search)
-
+   refreshResultBtnEl.addEventListener('click', function(e) {
+      console.log(resultsDiv);
+      // delete(resultsDiv);
+   })
 //-------------------------------------------
 
 
