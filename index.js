5d2c47708ca7da580c559dc0715dc586595a9a0c
Harry <hrgus3@gmail.com>
2022-01-04 15:46:58 -0500

    added spaces between variables, functions, etc. to make a bit more readable

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
       .then(resp => resp.json())
       .then((respObj) => { 
          respArray = respObj['releases'];
+         
          let songCardsContainer = document.querySelector('#songCardsContainer');
          songCardsContainer.innerHTML = " ";
          // console.log(respArray);
@@ -23,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
          if(resp === " "){
             const errorMessageHome = document.createElement('div');
             errorMessageHome.id('errorMessageHome');
+
             const errorMessage = document.createElement('p');
             errorMessage.id('errorMessage');
             errorMessage.textContent = error;
@@ -41,46 +43,59 @@ document.addEventListener('DOMContentLoaded', function() {
          saveSongBtn.setAttribute('class', 'saveSongBtn')
          saveSongBtn.textContent = '+';
          saveSongBtn.addEventListener('click', clearSongCardsContainer)
+
       let releaseDate = document.createElement('p');
          releaseDate.textContent = 'Release Date: ' + result.date;
+
       let songTitle = document.createElement('p');
          songTitle.textContent = 'Song Title: ' + result.title;
+
       let artistName = document.createElement('h4');
          artistName.textContent = result['artist-credit'][0].artist.name + "  "; 
+         
       let songList = document.createElement('li');
          songList.className = 'song';
          songList.textContent = artistName.innerText;
+
       let songCard = document.createElement('ul');
          songCard.className = 'songCard'
          songCard.appendChild(saveSongBtn);
          songList.appendChild(songTitle);
          songList.appendChild(releaseDate);
          songCard.appendChild(songList);
+         
       let songCardHouse = document.createElement('div');
          songCardHouse.setAttribute('class', 'songCardHouseDiv');
          songCardHouse.appendChild(songCard);
          songCardHouse.appendChild(saveSongBtn);
+
       let songCardsContainer = document.querySelector('#songCardsContainer');
          songCardsContainer.appendChild(songCardHouse);
+
       let body = document.querySelector('body');
          body.appendChild(songCardsContainer);
+
       let savedSongsList = songCardHouse.cloneNode(true);
          savedSongsList.className = 'savedSongsList';
       
       function createSavedSongsList(e) {
          let savedSongsContainer = document.querySelector('#savedSongs');
             savedSongsContainer.appendChild(savedSongsList);
+
          const unSaveSongBtn = document.createElement('button');
-            const btnToKill = savedSongsList.childNodes[1];
+
+         const btnToKill = savedSongsList.childNodes[1];
             btnToKill.parentNode.removeChild( btnToKill );
             unSaveSongBtn.className = 'unsavedSongBtn';
             unSaveSongBtn.textContent = "-";
+           
             unSaveSongBtn.addEventListener('click', (e) => unSaveSongFunc(btnToKill));
             savedSongsList.appendChild(unSaveSongBtn);
         
          function unSaveSongFunc(btn) {
             savedSongsList.removeChild(unSaveSongBtn);
             savedSongsList.appendChild(btn)
+
             savedSongsContainer.removeChild(savedSongsList);
             songCardsContainer.prepend(songCardHouse);
          }
